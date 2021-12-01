ef79ea0bf86747786c95dedc8dd92f761f8088b6
Yuval Bubnovsky <yuval.bubnov@gmail.com>
2021-12-01 16:19:19 +0200

    added MultiThread webserver

--- Assignment 3/MultiThreadedWebServer.py
+++ Assignment 3/MultiThreadedWebServer.py
@@ -1,34 +1,47 @@
 from socket import *
-import threading
+from threading import Thread
 
-stop = threading.Event()
 serverPort = 6768
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(4)
+print("Ready to server on port: " + str(serverPort))
 
 
 def handler(connection, addr):
-    print(f"A New Connection Has Been Established: {addr} is connected\n")
-    connected = True
-    while connected:
-        message = connection.recv(2048).decode()
-        if stop.is_set():
-            break
-    connection.close()
-
-
-def main():
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('', serverPort))
-    serverSocket.listen()
-
     while True:
+        try:
+            data = connection.recv(2048).decode()
+            if not data:
+                break
+            filename = data.split()[1]
+            f = open(filename[1:])
+            data = f.read()
+            print("Recieved Message")
+            connection.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+            for i in range(0, len(data)):
+                connection.send(data[i].encode())
+            connection.send("\r\n".encode())
+            connection.close()
+            print("Connection Closed!")
+            break
+        except IOError:
+            # Send response message for file not found
+            connection.send("HTTP/1.1 404 Not found\r\n\r\n".encode())
+            connection.send("<html><head></head><body><h1>404 Not found</h1></body></html>\r\n".encode())
+            # Close client socket
+            connection.close()
+            break
+
+
+while True:
+    try:
         connection, addr = serverSocket.accept()
-        thread = threading.Thread(target=handler, args=(connection, addr))
-        thread.start()
-        print(f"Number Of Threads : {threading.active_count() - 1}\n")
+        print("Incoming Connection From: " + str(addr))
+        t = Thread(target=handler, args=(connection, addr))
+        t.start()
+    except OSError:
+        break
 
-
-if __name__ == "__main__":
-    main_thread = threading.Thread(target=main)
-    main_thread.start()
-    main_thread.join(timeout=5)
-    stop.set()
+serverSocket.close()
