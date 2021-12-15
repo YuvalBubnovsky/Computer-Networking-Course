bf99baf0e9eb4d8ead8bceb42da1ab842917d5b2
YuvalBubnovsky <yuval.bubnovsky@gmail.com>
2021-12-15 23:26:48 +0200

    sender is done, compiling with no errors - test needed

--- Assignment 4/create_large_file.py
+++ Assignment 4/create_large_file.py
@@ -1,3 +1,3 @@
-with open("1gb.txt", "w") as out:
-    out.seek((1024 * 1024) - 1)
+with open("100mb.txt", "w") as out:
+    out.seek((1024 * 1024 * 100) - 1)
     out.write('\0')
--- Assignment 4/sender.c
+++ Assignment 4/sender.c
@@ -1,19 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <sys/socket.h>
+#include <sys/types.h>
+
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 
-int main(int argc, char **argv)
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include <fcntl.h>
+
+#define PORT 6769
+#define ADDRESS "127.0.0.1"
+#define SIZE 104857600 // 1024 * 1024 * 100 = 100MB of data
+
+void send_file(FILE *fp, int sock); // declaring this function for later use, no header file needed here
+
+int main()
 {
+    // creating a TCP socket
+    int sock;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = inet_addr(ADDRESS);
+
+    // setting up buffer for CC algo switch later on
     char buf[256];
     socklen_t len;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
+
+    // setting the socket to be non-blocking
+    fcntl(sock, F_SETFL, O_NONBLOCK);
+
+    // defining file pointers
+    FILE *fp;
+    char *filename = "100mb.txt";
+
+    // connecting to server and making sure connection is succuessful
+    int conn_status = connect(sock, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (conn_status < 0)
     {
-        perror("socket");
-        return -1;
+        printf("%s", "ERROR: Connection was NOT succuessful");
+        exit(1);
     }
 
     len = sizeof(buf);
@@ -25,6 +57,21 @@ int main(int argc, char **argv)
 
     printf("Current: %s\n", buf);
 
+    // Sending the file 5 times using the Cubic CC algorithm
+    for (int i = 0; i <= 4; i++)
+    {
+        fp = fopen(filename, "r");
+        if (fp == NULL)
+        {
+            printf("ERROR: File read unsuccuessful");
+            exit(1);
+        }
+        send_file(fp, sock);
+    }
+    printf("Sent Data 5 times using cubic CC algorithm");
+
+    // Switching the CC algorithm to be Reno
+    // The code bit for switching algorithms is courtesy of StackOverflow
     strcpy(buf, "reno");
     len = strlen(buf);
     if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0)
@@ -39,6 +86,35 @@ int main(int argc, char **argv)
         return -1;
     }
     printf("New: %s\n", buf);
+
+    // Sending the file 5 times using the Reno CC algorithm
+    for (int i = 0; i <= 4; i++)
+    {
+        fp = fopen(filename, "r");
+        if (fp == NULL)
+        {
+            printf("ERROR: File read unsuccuessful");
+            exit(1);
+        }
+        send_file(fp, sock);
+    }
+    printf("Sent Data 5 times using reno CC algorithm");
+
+    // Closing the socket
     close(sock);
     return 0;
+}
+
+void send_file(FILE *fp, int sock)
+{
+    char data[SIZE] = {0};
+    while (fgets(data, SIZE, fp) != NULL)
+    {
+        if (send(sock, data, sizeof(data), 0) == -1)
+        {
+            printf("ERROR: File send unsuccuessful");
+            exit(1);
+        }
+        bzero(data, SIZE);
+    }
 }
